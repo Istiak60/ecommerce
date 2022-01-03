615d88cfbe519d82f5aaa637d3df74bea3ae1184
Istiak60 <istisquare@gmail.com>
2022-01-03 22:05:43 +0600

    separating views

---
+++ php/public/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<?php
+include_once('../views/elements/head.php');
+
+?>
+<body>
+<?php
+include_once('../views/elements/header.php');
+include_once('../views/elements/carousel.php');
+include_once('../views/elements/best-sellers.php');
+include_once('../views/elements/special-products.php');
+include_once('../views/elements/special-products.php');
+include_once('../views/elements/script.php');
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ php/views/elements/header.php
@@ -0,0 +1,28 @@
+<?php?>
+
+<!--Markup for Header-->
+<header>
+        <div class="container">
+            <div class="d-none d-sm-none d-md-block">
+                <div class="row">
+                    <div class="col-sm-3 my-3">
+                        <a class="navbar-brand" href="index.html">
+                            <img src="img/logo.png" alt="Logo Icon">
+                        </a>
+                    </div>
+                   
+                </div>
+            </div>
+            <div class="d-block d-sm-block d-md-none">
+                <div class="row">
+                    <div class="col-sm-3 offset-4">
+                        <a class="navbar-brand" href="index.html">
+                            <img src="img/logo.png" alt="Logo Icon">
+                        </a>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
